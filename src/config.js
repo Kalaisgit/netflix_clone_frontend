8af1ff936fb4e854fca909a05232ecc24fa06a43
KALAISELVAN CK <posttokalai@gmail.com>
2024-11-12 18:28:57 +0530

    changed to deploy in vercel

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://netflix-clone-backend-l00x.onrender.com/api";
+  "https://netflixclonebackend-7d1n25mnb-kalaiselvan-cks-projects.vercel.app";
 
 export default API_BASE_URL;
