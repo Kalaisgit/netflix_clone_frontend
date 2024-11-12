4c088945bd8ded17592925a1896003855a9efab9
KALAISELVAN CK <posttokalai@gmail.com>
2024-11-12 18:35:38 +0530

    Changes made to config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://netflixclonebackend-7d1n25mnb-kalaiselvan-cks-projects.vercel.app";
+  "https://netflixclonebackend-hrfqwuvaz-kalaiselvan-cks-projects.vercel.app";
 
 export default API_BASE_URL;
